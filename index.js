857c1dd2fa76c2b6ebd0556614aebd22b4dcd69c
petersamson-kingori <peters.kingori@student.moringaschool.com>
2022-12-07 12:19:01 +0300

    commit tree 2eb6c804970c2c29e42f1e277e82e45c32c03631

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+
+};
+saturdayFun();
+
+const mondayWork = function( activity = 'go to the office'){
+return `This Monday, I will ${activity}.`
+};
+
+mondayWork("work from home");
+
+// const wrapAdjective = function(char = "*") {
+//     const part1 = "You are ";
+//     return function(name = "special") {
+//       const encouragingpromptFunction = (`${char}${name}${char}!`);
+//       return function() {
+//         return `${part1} ${encouragingpromptFunction}`;
+//       };
+  
+//     };
+//   };
+  
+//   wrapAdjective("*")("a hard worker")();
+
+const wrapAdjective  = function(adjective="special"){
+  return function(spac="*"){
+        return `You are ${adjective}${spac}${adjective}!`;
+ }
+}
+
+
+  
+
